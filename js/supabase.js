793294b03a1fb299747368480a741c2c3f3d9c45
Caio Belmont <caiohenriquebel@gmail.com>
2026-09-02 14:32:39 -0300

    Update system colors

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://jgigvjkyezvdffvbefnp.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnaWd2amt5ZXp2ZGZmdmJlZm5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NjYyMzMsImV4cCI6MjEwMzM0MjIzM30.4HiEyFtG7vlB29pJH_cgJklTU8mwqzcXiyGBYxjmF9k";
+const SUPABASE_URL = "https://buypoclnuewxbdfmakji.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eXBvY2xudWV3eGJkZm1ha2ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNjQwMDMsImV4cCI6MjEwMzk0MDAwM30.SXcEuCkZfDO280pScY1E5YBx4OIZTKdoA3h99MOXNic";
 
 export const supabase = createClient(
   SUPABASE_URL,
